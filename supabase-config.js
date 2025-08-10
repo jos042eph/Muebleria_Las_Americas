7f8542dd3f8f44a3f6693b53e3bb3e37b0f84ba7
jos042eph <joseph.sanchez.quiros0278@gmail.com>
2025-08-09 18:40:20 -0600

    supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,10 +1,8 @@
-<!-- supabase-config.js -->
-<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
-<script>
-  // REEMPLAZA con tus datos de Settings → API
-  const SUPABASE_URL = "https://TU-PROYECTO.supabase.co";
-  const SUPABASE_ANON_KEY = "TU-ANON-KEY";
+// supabase-config.js
 
-  // Cliente global
-  window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-</script>
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://vffoqlajyqgaxqshccir.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmZm9xbGFqeXFnYXhxc2hjY2lyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3ODEwNDUsImV4cCI6MjA3MDM1NzA0NX0.x2YbhCZrejrbIlrpPQLcXRF4hoRHwKNU2soRgv5grsA"
+);
